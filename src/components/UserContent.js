e2d1dba5fd9776ddd4ea068d300453509e7c5bf6
dg0mezm <dg0mezm@outlook.es>
2023-12-29 16:26:43 +0100

    Update users

--- src/components/UserContent.js
+++ src/components/UserContent.js
@@ -7,11 +7,7 @@ export default function UserContent() {
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
-
-        // Realizar la llamada a la API para obtener los datos del usuario
-        // Puedes utilizar fetch o tu biblioteca de manejo de HTTP preferida
         try {
-            // Simulando una llamada a la API (reemplaza esto con tu lógica real)
             const response = await fetch(`https://pre.dg0mezm.es/api/users/${userId}`);
             const data = await response.json();
             setUserData(data);
@@ -20,12 +16,47 @@ export default function UserContent() {
         }
     };
 
+    const getDateFormatted = (data) => {
+        let date = new Date(Date.parse(data))
+        const result = date.toLocaleDateString("es-ES", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit"
+        })
+
+        return result
+    }
+
+    const getCompleteDateFormatted = (data) => {
+        let date = new Date(Date.parse(data))
+        const result = date.toLocaleDateString("es-ES", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit"
+        })
+
+        return result
+    }
+
+    const refreshMiners = async () => {
+        if (userData) {
+            const userId = userData.user[0].public_profile_link;
+            try {
+                const response = await fetch(`https://pre.dg0mezm.es/api/users/${userId}/refresh/miners`);
+                await response.json();
+            } catch (error) {
+                console.error("Error al obtener datos del usuario:", error);
+            }
+        }
+    }
+
     return (
         <Container className="content-container">
             <Row className="justify-content-center col-12 m-auto">
                 <Col className="col-lg-8 col-xl-6">
-                    {/* Formulario y datos del usuario en la misma línea */}
-                    <Form onSubmit={handleFormSubmit} className="d-flex align-items-center gap-2 justify-content-center p-2">
+                    <Form onSubmit={handleFormSubmit} className="d-flex align-items-center gap-2 justify-content-center">
                         <Form.Group className="w-100">
                             <Form.Control
                                 type="text"
@@ -41,9 +72,28 @@ export default function UserContent() {
                 </Col>
             </Row>
             {userData && (
-                <div className="ms-3">
-                    <h2>Nombre del Usuario: {userData.user[0].fullname}</h2>
-                </div>
+                <Row className="justify-content-center col-12 m-auto">
+                    <Col>
+                        <Row>
+                            <div>Imagen</div>
+                        </Row>
+                        <Row>
+                            <Col className="align-items-center gap-2 justify-content-center">
+                                <div>Nombre: {userData.user[0].fullname}</div>
+                                <div>Género: {userData.user[0].sex.toUpperCase() === 'MALE' ? "Hombre" : "Mujer"}</div>
+                                <div>Fecha de registro: {getDateFormatted(userData.user[0].register_since_time)}</div>
+                                <div>Última actualización: {getCompleteDateFormatted(userData.user[0].last_refresh_user_info)}</div>
+                                <div>Última actualización de mineros: {getCompleteDateFormatted(userData.user[0].last_refresh_owned_miners)}</div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className="d-flex align-items-center gap-2 justify-content-center">
+                                <Button className="" onClick={refreshMiners}>Actualizar Mineros</Button>
+                                <Button className="">Actualizar Información</Button>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
             )}
         </Container>
     )
