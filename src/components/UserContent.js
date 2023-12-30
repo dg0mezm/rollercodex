5f640fb8edb678c69dc7615bd6e611015b6e5d6d
dg0mezm <dg0mezm@outlook.es>
2023-12-30 10:58:01 +0100

    Added Power on User Page

--- src/components/UserContent.js
+++ src/components/UserContent.js
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import { Button, Col, Container, Row, Form } from "react-bootstrap";
 
+const hasPassed15Minutes = (lastRefresh) => {
+    const fifteenMinutesInMillis = 15 * 60 * 1000; // 15 minutos en milisegundos
+    const now = new Date().getTime();
+    const lastRefreshTime = new Date(lastRefresh).getTime();
+
+    return now - lastRefreshTime > fifteenMinutesInMillis;
+};
+
 export default function UserContent() {
     const [userId, setUserId] = useState("");
     const [userData, setUserData] = useState(null);
@@ -42,13 +50,32 @@ export default function UserContent() {
 
     const refreshMiners = async () => {
         if (userData) {
-            const userId = userData.public_profile_link;
-            try {
-                const response = await fetch(`https://pre.dg0mezm.es/api/users/${userId}/refresh/miners`);
-                const data = await response.json();
-                data.data[0] ? setUserData(data.data[0]) : setUserData(null);
-            } catch (error) {
-                console.error("Error al obtener datos del usuario:", error);
+            if (hasPassed15Minutes(userData.last_refresh_owned_miners)) {
+                try {
+                    const response = await fetch(`https://pre.dg0mezm.es/api/users/${userId}/refresh/miners`);
+                    const data = await response.json();
+                    data.data[0] ? setUserData(data.data[0]) : setUserData(null);
+                } catch (error) {
+                    console.error("Error al obtener datos del usuario:", error);
+                }
+            } else {
+                alert('ATENCIÓN: Solo se puede refrescar una vez cada 15 minutos.')
+            }
+        }
+    }
+
+    const refreshUserInfo = async () => {
+        if (userData) {
+            if (hasPassed15Minutes(userData.last_refresh_user_info)) {
+                try {
+                    const response = await fetch(`https://pre.dg0mezm.es/api/users/${userId}/refresh/info`);
+                    const data = await response.json();
+                    data.data[0] ? setUserData(data.data[0]) : setUserData(null);
+                } catch (error) {
+                    console.error("Error al obtener datos del usuario:", error);
+                }
+            } else {
+                alert('ATENCIÓN: Solo se puede refrescar una vez cada 15 minutos.')
             }
         }
     }
@@ -79,6 +106,7 @@ export default function UserContent() {
                             <Col className="col-12">
                                 <div>Nombre: {userData.fullname}</div>
                                 <div>Género: {userData.sex.toUpperCase() === 'MALE' ? "Hombre" : "Mujer"}</div>
+                                <div>Poder: {userData.power}</div>
                                 <div>Fecha de registro: {getDateFormatted(userData.register_since_time)}</div>
                                 <div>Última actualización: {getCompleteDateFormatted(userData.last_refresh_user_info)}</div>
                                 <div>Última actualización de mineros: {getCompleteDateFormatted(userData.last_refresh_owned_miners)}</div>
@@ -87,7 +115,7 @@ export default function UserContent() {
                                 <Row className="mt-2">
                                     <Col className="d-flex align-items-center gap-2 justify-content-center">
                                         <Button className="p-1" onClick={refreshMiners}>Actualizar Mineros</Button>
-                                        <Button className="p-1">Actualizar Información</Button>
+                                        <Button className="p-1" onClick={refreshUserInfo}>Actualizar Información</Button>
                                     </Col>
                                 </Row>
                             </Col>
